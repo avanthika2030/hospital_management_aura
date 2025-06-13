6e8209d06e051dd648836c17cc10dd0e110f52f5
avanthika2030 <avanthikasha2006@gmail.com>
2025-06-13 17:55:22 +0530

    Client secret fix

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ import json
 import os
 from simplegmail import Gmail
 app = Flask(__name__)
-#app.secret_key = 'client_secret.json'
+app.secret_key = 'client_secret.json'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
